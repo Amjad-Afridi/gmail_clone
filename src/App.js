28e335693bf2c5ed7998d89ee90b94ac3bb7b4bb
Amjad Afridi <amjadafridi067@gmail.com>
2024-01-08 22:58:46 +0500

    commited changes

--- src/App.js
+++ src/App.js
@@ -1,89 +1,108 @@
-
-import React, {useEffect, useState} from 'react';
-import {useSelector, useDispatch} from "react-redux";
-import {GoogleLogin, googleLogout, useGoogleLogin} from '@react-oauth/google';
-import {loginInfo, logoutInfo} from "./Redux/Slices/UserSlice";
-import {UserProfile} from "./Redux/Thunks/UserProfile";
+import ReactDOMServer from "react-dom/server";
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { GoogleLogin, googleLogout, useGoogleLogin } from "@react-oauth/google";
+import { loginInfo, logoutInfo } from "./Redux/Slices/UserSlice";
+import { UserProfile } from "./Redux/Thunks/UserProfile";
 import axios from "axios";
 import Sidebar from "./components/Sidebar";
 
 function App() {
-    const dispatch = useDispatch()
-    const [userToken, setUserToken] = useState({})
-    const {token, error, loading, profile} = useSelector((state) => state.user)
+  const dispatch = useDispatch();
+  const [htmlContent, setHtmlContent] = useState([]);
+  const [userToken, setUserToken] = useState({});
+  const { token, error, loading, profile } = useSelector((state) => state.user);
 
-    const login = useGoogleLogin({
-        onSuccess: (codeResponse) => {
-            setUserToken(codeResponse)
-        },
-        onError: (error) => console.log('Login Failed:', error),
-        scope: 'https://www.googleapis.com/auth/gmail.readonly'
-    })
-    const handleLogout = () => {
-        setUserToken({})
-        dispatch(logoutInfo())
+  const login = useGoogleLogin({
+    onSuccess: (codeResponse) => {
+      setUserToken(codeResponse);
+    },
+    onError: (error) => console.log("Login Failed:", error),
+    scope: "https://www.googleapis.com/auth/gmail.readonly",
+  });
+  const handleLogout = () => {
+    setUserToken({});
+    dispatch(logoutInfo());
+  };
+  const getProfile = async () => {
+    const result = await dispatch(UserProfile(token));
+  };
+  const searchMessages = async () => {
+    if (profile) {
+      //   console.log(profile);
+      //   console.log("token is: ", token);
+      try {
+        const result = await axios.get(
+          `https://gmail.googleapis.com/gmail/v1/users/${profile.email}/messages`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+              Accept: "application/json",
+            },
+            params: {
+              maxResults: 15,
+            },
+          }
+        );
+        const messages = result.data.messages.map(async ({ id }) => {
+          await axios
+            .get(
+              `https://gmail.googleapis.com/gmail/v1/users/${profile.email}/messages/${id}`,
+              {
+                headers: {
+                  Authorization: `Bearer ${token}`,
+                  Accept: "application/json",
+                },
+                params: {
+                  format: "full",
+                },
+              }
+            )
+            .then((response) => {
+              response.data.payload.parts.forEach((part) => {
+                if (
+                  part.mimeType === "text/plain" ||
+                  part.mimeType === "text/html"
+                ) {
+                  const content = part.body.data;
+                  const safeContent = content
+                    .replace(/-/g, "+")
+                    .replace(/_/g, "/");
+                  const decodedContent = atob(safeContent);
+                  setHtmlContent([...htmlContent, decodedContent]);
+                }
+              });
+            })
+            .catch((e) => e.message);
+        });
+      } catch (e) {
+        console.log(e.message);
+      }
     }
-    const getProfile = async () => {
-        const result = await dispatch(UserProfile(token))
-    }
-    const searchMessages = async () => {
-        if (profile) {
-            console.log(profile)
-            console.log('token is: ', token)
-            try {
-                const result = await axios.get(`https://gmail.googleapis.com/gmail/v1/users/${profile.email}/messages`, {
-                        headers: {
-                            Authorization: `Bearer ${token}`,
-                            Accept: 'application/json'
-                        },
-                        params: {
-                            maxResults: 5
-                        },
-                    }
-                )
-                const messages = result.data.messages.map(async ({id}) => {
-                    await axios.get(`https://gmail.googleapis.com/gmail/v1/users/${profile.email}/messages/${id}`, {
-                        headers: {
-                            Authorization: `Bearer ${token}`,
-                            Accept: 'application/json'
-                        },
-                        params: {
-                            format: "full"
-                        }
-                    }).then((response) => {
-                        // console.log(response.data.payload.parts)
-                        response.data.payload.parts.forEach(part => {
-                            if (part.mimeType === 'text/plain' || part.mimeType === 'text/html') {
-                                const content = part.body.data;
-                                const safeContent = content.replace(/-/g, '+').replace(/_/g, '/');
-                                console.log('content is ',atob(safeContent))
-                                // const decodedContent = atob(content);
-                                // console.log(decodedContent)
-                                // Process or display the content
-                            }
-                        })
-                    }).catch(e => e.message)
-                })
-            } catch (e) {
-                console.log(e.message)
-            }
-        }
-    }
-    if (userToken.access_token) {
-        dispatch(loginInfo(userToken.access_token))
-    }
+  };
+  if (userToken.access_token) {
+    dispatch(loginInfo(userToken.access_token));
+  }
 
-    return (
-        <>
-            <div> login with google</div>
-            {userToken.access_token ? <div>
-                <button onClick={handleLogout}>logout</button>
-                <button onClick={getProfile}>Get Profile</button>
-                <button onClick={searchMessages}>Search Messages</button>
-                {error && <p>{error.message}</p>}
-                {loading && <p>loading</p>}
-            </div> : <button onClick={() => login()}>login</button>}
-        </>
-    )
+  return (
+    <>
+      <div> login with google</div>
+      {userToken.access_token ? (
+        <div>
+          <button onClick={handleLogout}>logout</button>
+          <button onClick={getProfile}>Get Profile</button>
+          <button onClick={searchMessages}>Search Messages</button>
+          {error && <p>{error.message}</p>}
+          {loading && <p>loading</p>}
+        </div>
+      ) : (
+        <button onClick={() => login()}>login</button>
+      )}
+      {htmlContent.length > 0 &&
+        htmlContent.map((itemHtml) => {
+          return <div dangerouslySetInnerHTML={{ __html: itemHtml }} />;
+        })}
+    </>
+  );
 }
-export default App
+export default App;
